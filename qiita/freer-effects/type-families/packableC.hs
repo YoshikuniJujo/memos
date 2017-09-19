ded447b6434dee38f29e419e29d8b9b5ef063f43
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-19 14:23:15 +0900

    Freer Effects: 型族: 型クラスと関連づけられた型シノニム族

--- qiita/freer-effects/type-families/packableC.hs
+++ qiita/freer-effects/type-families/packableC.hs
@@ -10,22 +10,20 @@ import Data.Word
 
 import qualified Data.ByteString as BS
 
-type family List x
-
-type instance List () = Int
-type instance List Bool = (Int, Integer)
-type instance List Word8 = BS.ByteString
-type instance List Double = [Double]
-
 class Packable p where
+--	type List p
+	type family List p
 	fromList :: [p] -> List p
 	toList :: List p -> [p]
 
 instance Packable () where
+	type List () = Int
+--	type instance List () = Int
 	fromList = length
 	toList = (`replicate` ())
 
 instance Packable Bool where
+	type List Bool = (Int, Integer)
 	fromList = \case
 		[] -> (0, 0)
 		b : bs -> (+ 1) *** (bool 0 1 b .|.) . (`shiftL` 1)
@@ -34,13 +32,19 @@ instance Packable Bool where
 	toList (l, n) = n `testBit` 0 : toList (l - 1, n `shiftR` 1)
 
 instance Packable Word8 where
+	type List Word8 = BS.ByteString
 	fromList = BS.pack
 	toList = BS.unpack
 
 instance Packable Double where
+	type List Double = [Double]
 	fromList = id
 	toList = id
+
+{-
+type instance List Int = [Int]
 
 instance Packable Int where
 	fromList = id
 	toList = id
+	-}
